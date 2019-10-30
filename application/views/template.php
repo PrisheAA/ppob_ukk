3f7c2b4d93f415b0b707e52920278e94f11762ea
Prishe Annadjar <prishe_annadjar_26rpl@student.smktelkom-mlg.sch.id>
2019-10-30 08:04:51 +0700

    First Commit

--- application/views/template.php
+++ application/views/template.php
@@ -280,7 +280,7 @@
                 <img src="<?=base_url()?>assets/images/user.png" width="48" height="48" alt="User" />
                 </div>
                 <div class="info-container">
-                    <div class="name" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Risma</div>
+                    <div class="name" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Prishe</div>
                     <div class="email">admin</div>
                     <div class="btn-group user-helper-dropdown">
                         <i class="material-icons" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">keyboard_arrow_down</i>
